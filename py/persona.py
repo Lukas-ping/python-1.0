ca52300b6aa18a19587cc1bcf0fa340608c788dd
Lukas-ping <995qq58mn5@privaterelay.appleid.com>
2026-01-28 16:32:45 -0300

    Add files via upload

---
+++ py/persona.py
@@ -0,0 +1,32 @@
+class hijo:
+    def __init__(self,nombre,edad,signo,lugar_de_nacimiento,Coche_favorito):
+       self.nombre = "lukas"
+       self.edad = "18"
+       self.signo = "acuario"
+       self.lugar_de_nacimiento= "Argentina"
+       self.coche_favorito= "Porsche 918 spyder"
+
+    def saludar(self):
+        print("Hola, soy", self.nombre)
+
+    def miedad(self):
+        print("mi edad es", self.edad)
+    
+    def misigno(self):
+        print("mi signo es", self.signo)
+    
+    def milugar_de_nacimiento(self):
+        print("lugar de nacimiento es", self.lugar_de_nacimiento)
+
+    def micochefavorito(self):
+        print("mi coche favorito es ", self.coche_favorito)
+
+
+    
+hijo = int (input("que quieres saber de mi?"))
+
+hijo = int (input("mi signo , mi nombre , mi edad , mi signo , mi lugar de nacimiento , mi coche favorito"))
+
+
+if saber_que == "mi signo":
+  print ("mi signo es ", self )
